d764fea8492e9e3c27d3fbdbad2af464d72043eb
“Ebenezeri <cebendemalek@gmail.com>
2024-03-25 12:33:00 +0300

    commit

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../models/shoe.dart';
 
+// ignore: must_be_immutable
 class ShoeTile extends StatelessWidget {
     Shoe shoe;
     ShoeTile({super.key, required this.shoe});
